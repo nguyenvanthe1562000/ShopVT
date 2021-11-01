8d452eaa8d92d785b96e8668569aba82c5177eef
nguyenvanthe1562000 <nguyenvanthe1562000@gmail.com>
2021-11-01 15:43:38 +0700

    no message

---
+++ ShopVT/Data/Repository/Interface/B00AppUserinterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common;
+using Model.Model;
+namespace Data.Reponsitory.Interface
+
+{
+    public interface IB00AppUserRepository
+    {
+    	Task<bool> Insert( B00AppUserModel model, int userId);            
+
+	Task<bool> Update( B00AppUserModel model, int userId);         
+
+	 Task<bool>  Delete( string code, int userId);
+
+
+	Task<List<B00AppUserModel>> GetAll();
+
+	Task<List<B00AppUserModel>> Search();
+  
+
+
+	  Task<PagedResultBase> Paging(PagingRequestBase pagingRequest);
+
+	Task<B00AppUserModel> GetById(string code);
+
+
+    }
+}
+
+
+
+
+
---
+++ ShopVT/Data/Repository/Interface/B10PostCategoryinterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common;
+using Model.Model;
+namespace Data.Reponsitory.Interface
+
+{
+    public interface IB10PostCategoryRepository
+    {
+    	Task<bool> Insert( B10PostCategoryModel model, int userId);            
+
+	Task<bool> Update( B10PostCategoryModel model, int userId);         
+
+	 Task<bool>  Delete( string code, int userId);
+
+
+	Task<List<B10PostCategoryModel>> GetAll();
+
+	Task<List<B10PostCategoryModel>> Search(string Name);
+  
+
+
+	  Task<PagedResultBase> Paging(PagingRequestBase pagingRequest);
+
+	Task<B10PostCategoryModel> GetById(string code);
+
+
+    }
+}
+
+
+
+
+
