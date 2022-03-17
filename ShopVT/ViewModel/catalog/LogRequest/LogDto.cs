123a26fdd276c57b95dcc4f60197ee7d07168b25
TheNV <nguyenvanthe1562000@gmail.com>
2022-03-17 15:56:31 +0700

    the commit

---
+++ ShopVT/ViewModel/catalog/LogRequest/LogDto.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System;
+
+namespace API.Dto.Admin.ServiceDtos
+{
+    public class FilterLogDto
+    {
+        public string FileName { get; set; }
+        public string ClassName { get; set; }
+        public string MethodName { get; set; }
+        public int? LineNumber { get; set; }
+        public LogType? Type { get; set; }
+        public DateTime? LogTimeFrom { get; set; }
+        public DateTime? LogTimeTo { get; set; }
+        public string Message { get; set; }
+    }
+}
